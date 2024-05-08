d9d57fe9985cb30ba0bbe0bf9731aea6aa27f000
josehtat <150950561+josehtat@users.noreply.github.com>
2024-05-08 18:51:37 +0200

    Add files via upload

---
+++ M3/Planet_Wars/src/PlanetWars/LigthHunter.java
@@ -0,0 +1,10 @@
+package PlanetWars;
+
+public class LigthHunter extends Ship{
+
+	public LigthHunter(int armor, int baseDAmage) {
+		super(armor, baseDAmage);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ M3/Planet_Wars/src/PlanetWars/Main.java
@@ -0,0 +1,7 @@
+package PlanetWars;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
