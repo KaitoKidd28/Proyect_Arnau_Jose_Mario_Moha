0fa12d3a727949932f3de40b02a6ee434b0bc888
Arnau <arnau.calvo6@gmail.com>
2024-05-08 23:52:31 +0200

    Creando clases

--- M3/src/proyecto/Planet.java
+++ M3/src/proyecto/Planet.java
@@ -1,7 +1,94 @@
 package proyecto;
 
+import java.util.ArrayList;
+
 public class Planet {
 	private int technologyDefense;
 	private int technologyAtack;
 	private int metal;
+	private int deuterium;
+	private int upgradeDefenseTechnologyDeuteriumCost;
+	private int upgradeAttackTechnologyDeuteriumCost;
+	ArrayList<MilitaryUnit>[] army = new ArrayList[7];
+	
+	private Planet(int technologyDefense, int technologyAtack, int metal, int deuterium,
+			int upgradeDefenseTechnologyDeuteriumCost, int upgradeAttackTechnologyDeuteriumCost,
+			ArrayList<MilitaryUnit>[] army) {
+		super();
+		this.technologyDefense = technologyDefense;
+		this.technologyAtack = technologyAtack;
+		this.metal = metal;
+		this.deuterium = deuterium;
+		this.upgradeDefenseTechnologyDeuteriumCost = upgradeDefenseTechnologyDeuteriumCost;
+		this.upgradeAttackTechnologyDeuteriumCost = upgradeAttackTechnologyDeuteriumCost;
+		this.army = army;
+	}
+
+	public int getTechnologyDefense() {
+		return technologyDefense;
+	}
+
+	public void setTechnologyDefense(int technologyDefense) {
+		this.technologyDefense = technologyDefense;
+	}
+
+	public int getTechnologyAtack() {
+		return technologyAtack;
+	}
+
+	public void setTechnologyAtack(int technologyAtack) {
+		this.technologyAtack = technologyAtack;
+	}
+
+	public int getMetal() {
+		return metal;
+	}
+
+	public void setMetal(int metal) {
+		this.metal = metal;
+	}
+
+	public int getDeuterium() {
+		return deuterium;
+	}
+
+	public void setDeuterium(int deuterium) {
+		this.deuterium = deuterium;
+	}
+
+	public int getUpgradeDefenseTechnologyDeuteriumCost() {
+		return upgradeDefenseTechnologyDeuteriumCost;
+	}
+
+	public void setUpgradeDefenseTechnologyDeuteriumCost(int upgradeDefenseTechnologyDeuteriumCost) {
+		this.upgradeDefenseTechnologyDeuteriumCost = upgradeDefenseTechnologyDeuteriumCost;
+	}
+
+	public int getUpgradeAttackTechnologyDeuteriumCost() {
+		return upgradeAttackTechnologyDeuteriumCost;
+	}
+
+	public void setUpgradeAttackTechnologyDeuteriumCost(int upgradeAttackTechnologyDeuteriumCost) {
+		this.upgradeAttackTechnologyDeuteriumCost = upgradeAttackTechnologyDeuteriumCost;
+	}
+
+	public ArrayList<MilitaryUnit>[] getArmy() {
+		return army;
+	}
+
+	public void setArmy(ArrayList<MilitaryUnit>[] army) {
+		this.army = army;
+	}
+	
+	public void upgradeTechnologyDefense(int upgradeDefenseTechnologyDeuteriumCost, int deuterium) {
+		try {
+			if (deuterium >= upgradeDefenseTechnologyDeuteriumCost) {
+				
+			}
+		}
+		finally {
+			
+		}
+	}
+	
 }
