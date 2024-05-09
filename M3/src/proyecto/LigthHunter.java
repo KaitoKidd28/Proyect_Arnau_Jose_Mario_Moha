a4c9ed1e585f94d00432f2e3c1e37afc0888ef76
Arnau <arnau.calvo6@gmail.com>
2024-05-10 00:52:56 +0200

    java

--- M3/src/proyecto/LigthHunter.java
+++ M3/src/proyecto/LigthHunter.java
@@ -1,5 +1,57 @@
 package proyecto;
 
-public class LigthHunter {
+public class LigthHunter extends Ship {
 
+	@Override
+	public int attack() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void tekeDamage(int receivedDamage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getActualArmor() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMetalCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDeuteriumCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getChanceGeneratinWaste() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getChanceAttackAgain() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void resetArmor() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
 }
--- M3/src/proyecto/Ship.java
+++ M3/src/proyecto/Ship.java
@@ -4,4 +4,17 @@ public abstract class Ship implements MilitaryUnit, Variables {
 	private int armor;
 	private int initialArmor;
 	private int baseDamage;
+	public Ship() {
+		super();
+	}
+	public Ship(int armor, int initialArmor, int baseDamage) {
+		super();
+		this.armor = armor;
+		this.initialArmor = initialArmor;
+		this.baseDamage = baseDamage;
+	}
+	
+	
+	
+	
 }
