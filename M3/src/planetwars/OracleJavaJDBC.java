43b88ec26df070bc36fb906c4ea122403c9e6335
josehtat <150950561+josehtat@users.noreply.github.com>
2024-05-12 16:30:36 +0200

    conexionOracle2

--- M3/src/planetwars/OracleJavaJDBC.java
+++ M3/src/planetwars/OracleJavaJDBC.java
@@ -7,30 +7,44 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class OracleJavaJDBC {
-	public static void main(String[] args) throws SQLException {
-		Connection conn = null;
-		Statement stmt = null;
-		String DB_URL = "jdbc:oracle:thin:@http://localhost:1521:orcl";
-		String USER = "system";
-		String PASS = "admin";
-		
-		try {
-			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        String DB_URL = "jdbc:oracle:thin:@localhost:1521:orcl";
+        String USER = "sys as sysdba";
+        String PASS = "P@ssw0rd";
+        // Creating Connection
+        try {
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
-			if (conn != null) {
-				System.out.println("Connected to the Oracle DB!");
-				stmt = conn.createStatement();
-			} else {
-				System.out.println("Failed to make connection!");
-			}
-		
-		} catch (SQLException e) {
-			System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			stmt.close();
-			conn.close();
-		}
-	}
+            if (conn != null) {
+                System.out.println("Connected to the Oracle DB!");
+                // Crear el Statement después de haber establecido la conexión
+                stmt = conn.createStatement();
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Intentar cerrar el Statement y la conexión en el bloque finally
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
