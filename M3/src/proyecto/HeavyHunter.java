17d99e387db35b697adc339297a954d6f327aa2a
Arnau <arnau.calvo6@gmail.com>
2024-05-13 20:23:42 +0200

    Update HeavyHunter.java

--- M3/src/proyecto/HeavyHunter.java
+++ M3/src/proyecto/HeavyHunter.java
@@ -1,5 +1,58 @@
 package proyecto;
 
-public class HeavyHunter {
+public class HeavyHunter extends Ship {
 
+	@Override
+	public int attack() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void tekeDamage(int receivedDamage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getActualArmor() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMetalCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDeuteriumCost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getChanceGeneratinWaste() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getChanceAttackAgain() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void resetArmor() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public HeavyHunter(int armor, int baseDamage) {
+		super(armor, baseDamage);
+	}
+	
+	
 }
